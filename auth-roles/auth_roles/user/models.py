85094e39ba9526037e80592af94f9f9bdef2e03b
devoure <disguisedsandwich@gmail.com>
2024-07-15 19:43:19 +0300

    Added base user inheritance

--- auth-roles/auth_roles/user/models.py
+++ auth-roles/auth_roles/user/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth import AbstractBaseUser
 
-# Create your models here.
 
+# Create your models here.
+class User(AbstractBaseUser):
+    username = models.CharField(max_length=256)
+    ..
